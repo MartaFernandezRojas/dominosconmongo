2c1d40c3037d843f43b585654d7c9d8b18a9cbdc
Unknown <f.m.rojas@hotmail.com>
2019-01-29 17:52:44 +0100

    2901

--- FrontEnd/src/components/form/form.js
+++ FrontEnd/src/components/form/form.js
@@ -6,21 +6,41 @@ class Form extends Component {
 
     render() {
         return (
-            <div>
-                <form>
-                    <label for="nombre">Nombre:</label>
-                        <input className="form-control" id="nombre" type="text" name="nombre" />
-                        <label for="email">Email:</label>
-                        <input className="form-control" id="email" type="text" name="email" />
-                        <label>
-                            <input type="checkbox"> Recordarme
-                    </label>
-                            <button type="submit">Enviar</button>
-                </form>
-            </div>
-                )
-              }
-            };
-            
-            export default Form;
-  
+            <div className="container container2">
+                <div className="row">
+                    <div className="col-md-5">
+                        <div className="card">
+                            <h2>Registro de usuario</h2>
+                            <form className="form">
+                                <label for="nombre">Nombre:</label>
+                                <h5>Nombre</h5>
+                                <input className="form-control" id="nombre" type="text" name="nombre" placeholder="Nombre" />
+                                <h5>Apellido</h5>
+                                <input className="form-control" id="apellido" type="text" name="apellido" placeholder="Apellido" />
+                                <h5>Email</h5>
+                                <input className="form-control" id="email" type="text" name="email" placeholder="Email" />
+                                <h5>Password</h5>
+                                <input className="form-control" id="password" type="password" name="password" placeholder="Password" />
+                                <button className="btn btn-primary" type="submit">Enviar</button>
+                            </form>
+                        </div>
+                    </div>
+                    <div className="col-md-5">
+                        <div className="card">
+                            <h2>LogIn</h2>
+                            <form className="form">
+                                <h5>Email</h5>
+                                <input className="form-control" id="email" type="text" name="email" placeholder="Email" />
+                                <h5>Password</h5>
+                                <input className="form-control" id="password" type="password" name="password" placeholder="Password" />
+                                <button className="btn btn-primary" type="submit">Enviar</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                </div>
+        )
+    }
+};
+
+export default Form;
